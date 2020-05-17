bc180eebdcd930f1075f274f2450a23a53d1fc72
sunshineAG <spirit_xf@163.com>
2020-05-17 22:13:53 +0800

    up

---
+++ src/components/SiteSearch.vue
@@ -0,0 +1,54 @@
+
+<style lang="less" scoped>
+    
+</style>
+
+<template>
+
+    <div class="site-search-main">
+        
+        <div >这里是站内搜索
+
+            <div @click="test">
+                <van-search v-model="value" placeholder="请输入搜索关键词" />
+            </div>
+        </div>
+        
+    </div>
+
+</template>
+
+<script>
+import {Search } from "vant"
+export default {
+    name:'SiteSearch',
+    components:{
+        [Search.name]:Search
+    },
+    data(){
+        return{
+            value:''
+        }
+    },
+    methods:{
+        test(){
+            this.$router.push('/FilterSearch')
+        },
+        httpRequest(){
+            
+            // this.$axios({
+            //     method:'POST',
+            //     url:"",
+            //     data:{}
+            // }).then(res=>{
+
+            // }).catch(error=>{
+
+            //}).finally(() => {
+            //       
+            //    })
+
+        }
+    }
+}
+</script>
---
+++ src/components/SourceRemixer.vue
@@ -0,0 +1,48 @@
+
+<style lang="less" scoped>
+    .source-remixer-main{
+        height:2000px;
+        .remixer-adImg{
+            width:110%;
+            height:420px;
+            position: absolute;
+            top: 0;
+        }
+    }
+</style>
+
+<template>
+
+    <div class="source-remixer-main">
+        <div class="test" style="margin-left:-10px;width:104%;border-radius: 100% 100% 100% 100% / 0% 0% 24% 24%;overflow:hidden;position:relative;height:200px;background:#fff;">
+            <img src="https://oktools.net/ph/400x320" alt="" class="remixer-adImg" >
+        </div>
+        <van-sticky @scroll="getInfoData($event)">
+            <div v-if='hideFlag'>
+                我是隐藏的组件吖
+            </div>
+            <div>主播</div>
+        </van-sticky>
+    </div>
+
+</template>
+
+<script>
+import {Sticky} from "vant"
+export default {
+    name:'sourceInfo',
+    components:{
+        [Sticky.name]:Sticky
+    },
+    data(){
+        return{
+            hideFlag:false
+        }
+    },
+    methods:{
+        getInfoData(e){
+            this.hideFlag = e.isFixed
+        }
+    }
+}
+</script>
